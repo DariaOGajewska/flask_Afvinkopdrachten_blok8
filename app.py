df16271361504c688f93a327cd2fa6f202a43bf3
daria <do.gajewska@student.han.nl>
2023-04-12 12:13:13 +0200

    continuing db connection

--- app.py
+++ app.py
@@ -1,18 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template, request
 import mysql.connector
 
 # Connect with the MySQL Server
-cnx = mysql.connector.connect(host="ensembldb.ensembl.org",
+db = mysql.connector.connect(host="ensembldb.ensembl.org",
                               user="anonymous",
                               password="",
                               db="homo_sapiens_core_95_38")
+
+# A cursor object that will execute all the queries that are needed
+cur = db.cursor()
+
+# print all the first cell of all the rows
+for row in cur.fetchall():
+    print(row[0])
+
+db.close()
+# Create the app
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world():
     return 'Hello World!'
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        cursor = mysql.connection.cursor("SELECT * FROM ")
+        cursor.execute()
+        cursor.fetchall()
+        cursor.close()
+        return f"Done!!"
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
