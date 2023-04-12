2681ee2be7046e28d4721259d535dfa094b9723c
daria <do.gajewska@student.han.nl>
2023-04-12 17:36:41 +0200

    continuing db execution

--- app.py
+++ app.py
@@ -7,31 +7,35 @@ db = mysql.connector.connect(host="ensembldb.ensembl.org",
                               password="",
                               db="homo_sapiens_core_95_38")
 
-# A cursor object that will execute all the queries that are needed
-cur = db.cursor()
+# # print all the first cell of all the rows
+# for row in cur.fetchall():
+#     print(row[0])
 
-# print all the first cell of all the rows
-for row in cur.fetchall():
-    print(row[0])
-
-db.close()
 # Create the app
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+# @app.route('/')
+# def hello_world():
+#     return render_template('base.html')
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        cursor = mysql.connection.cursor("SELECT * FROM ")
-        cursor.execute()
-        cursor.fetchall()
+    if request.method == 'GET':
+        search = request.form.get("search", "")
+        # A cursor object that will execute all the queries that are needed
+        cursor = db.cursor()
+        # list = cursor.execute("SELECT * FROM homo_sapiens_funcgen_95_38")
+        # # Fetching everything from the database
+        # for row in cursor.fetchall():
+        #     print(row)
+        # Closing the cursor
         cursor.close()
-        return f"Done!!"
+        db.close()
+        return render_template("base.html", search=search)
+    else:
+        return render_template("base.html", search="")
 
 if __name__ == '__main__':
     app.run(debug=True)
