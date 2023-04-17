5f2bc474c38e80953188584a764a56d447944cd6
daria <do.gajewska@student.han.nl>
2023-04-17 12:19:41 +0200

    search bar working without any problems

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
 from flask import Flask, render_template, request
 import mysql.connector
 
-# Connect with the MySQL Server
-db = mysql.connector.connect(host="ensembldb.ensembl.org",
-                              user="anonymous",
-                              password="",
-                              db="homo_sapiens_core_95_38")
-
 # # print all the first cell of all the rows
 # for row in cur.fetchall():
 #     print(row[0])
@@ -21,21 +15,36 @@ app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
-def login():
-    if request.method == 'GET':
-        search = request.form.get("search", "")
+def home():
+    # Connect with the MySQL Server
+    db = mysql.connector.connect(host="ensembldb.ensembl.org",
+                                 user="anonymous",
+                                 password="",
+                                 db="homo_sapiens_core_95_38")
+    if request.method == 'POST':
+        search = "%"+request.form.get("search", "")+"%"
         # A cursor object that will execute all the queries that are needed
         cursor = db.cursor()
-        # list = cursor.execute("SELECT * FROM homo_sapiens_funcgen_95_38")
-        # # Fetching everything from the database
-        # for row in cursor.fetchall():
-        #     print(row)
+        query = f"SELECT description FROM gene where description like %s"
+        cursor.execute(query, [search])
+        # Fetching everything from the database
+        fetched_curs = cursor.fetchall()
+        # Removing % from the search input
+        search = search.replace("%", "")
+        # Making a new datalist
+        dataset_list = []
+        for element in fetched_curs:
+            if search in element[0]:
+                new_string = element[0].replace(search, "<b>"+search+"</b>")
+                dataset_list.append(new_string)
+
+
         # Closing the cursor
         cursor.close()
         db.close()
-        return render_template("base.html", search=search)
+        return render_template("base.html", cursor=dataset_list)
     else:
-        return render_template("base.html", search="")
+        return render_template("base.html", cursor=[" ", " "])
 
 if __name__ == '__main__':
     app.run(debug=True)
